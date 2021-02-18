6930f812d4e3825f8a5cfb0255fcacc1217985d5
Christian Johansen <christian@cjohansen.no>
2021-02-18 21:58:45 +0100

    Add some tests

--- src/parrot/core.clj
+++ src/parrot/core.clj
@@ -31,16 +31,19 @@
 (defmethod match? :default [k spec req]
   (comparable? (k spec) (k req)))
 
-(defn matches-request? [req matcher]
-  (let [criteria (if (vector? matcher)
-                   {:method (first matcher)
-                    :url (second matcher)}
-                   matcher)]
-    (loop [ks (keys criteria)]
+(defn normalize-spec [s]
+  (if (vector? s)
+    {:method (first s)
+     :url (second s)}
+    s))
+
+(defn matches-request? [req spec]
+  (let [spec (normalize-spec spec)]
+    (loop [ks (keys spec)]
       (if (nil? ks)
         true
         (let [k (first ks)]
-          (if (match? k criteria req)
+          (if (match? k spec req)
             (recur (next ks))
             false))))))
 
@@ -62,13 +65,15 @@
     res))
 
 (defn stringify-request [spec & [req]]
-  (let [[method url specs] (or spec [(:method req :get) (:url req)])
-        matchers (dissoc specs :headers)]
+  (let [spec (normalize-spec spec)
+        method (:method spec (:method req))
+        url (:url spec (:url req))
+        matchers (dissoc spec :method :url :headers)]
     (str (when-not spec
            "[UNEXPECTED] ")
          (->> [(str (str/upper-case (name method)) " " url)
                (str/join "\n" (map #(str/join ": " %) (or (:headers req)
-                                                      (:headers specs))))
+                                                      (:headers spec))))
                (str/join "\n" (map #(str/join " " %) matchers))]
               (remove empty?)
               (str/join "\n"))
@@ -79,39 +84,39 @@
        (map #(str (str/join (repeat n " ")) %))
        (str/join "\n")))
 
-(defn pass [params]
-  (test/do-report (assoc params :type :pass))
-  true)
-
-(defn verify-all-responses-requested [request-log response-stubs & [msg]]
-  (let [provided-stubs (set (map first (partition 2 @response-stubs)))
-        requested-stubs (set (map :spec @request-log))
+(defn verify-request-log [request-log response-stubs & [msg]]
+  (let [provided-stubs (set (map first (partition 2 response-stubs)))
+        requested-stubs (set (map :spec request-log))
         stubs-not-requested (set/difference provided-stubs requested-stubs)]
     (if (empty? stubs-not-requested)
-      (pass {:message (str (when msg (str msg ": "))
-                           "All response stubs were requested")
-             :expected 'empty?
-             :actual stubs-not-requested})
-      (test/do-report
-       {:type :fail
-        :message (format (str (when msg (str msg ": "))
-                              "%d response stub%s not requested:\n"
-                              "%s\n\nReceived requests:\n%s\n")
-                         (count stubs-not-requested)
-                         (if (= 1 (count stubs-not-requested))
-                           " was"
-                           "s were")
-                         (->> stubs-not-requested
-                              (map #(stringify-request %))
-                              (str/join "\n")
-                              (indent 2))
-                         (->> @request-log
-                              (filter (comp nil? :spec))
-                              (map #(stringify-request (:spec %) (:req %)))
-                              (str/join "\n")
-                              (indent 2)))
-        :expected #{}
-        :actual stubs-not-requested}))))
+      {:type :pass
+       :message (str (when msg (str msg ": "))
+                     "All response stubs were requested")
+       :expected 'empty?
+       :actual stubs-not-requested}
+      {:type :fail
+       :message (format (str (when msg (str msg ": "))
+                             "%d response stub%s not requested:\n"
+                             "%s\n\nReceived requests:\n%s\n")
+                        (count stubs-not-requested)
+                        (if (= 1 (count stubs-not-requested))
+                          " was"
+                          "s were")
+                        (->> stubs-not-requested
+                             (map #(stringify-request %))
+                             (str/join "\n")
+                             (indent 2))
+                        (->> request-log
+                             (map #(stringify-request (:spec %) (:req %)))
+                             (str/join "\n")
+                             (indent 2)))
+       :expected #{}
+       :actual stubs-not-requested})))
+
+(defn verify-all-responses-requested [request-log response-stubs & [msg]]
+  (let [res (verify-request-log @request-log @response-stubs msg)]
+    (test/do-report res)
+    (= :pass (:type res))))
 
 (defn assert-all-responses-requested [& [msg]]
   (throw (Exception. "Wrap test code in with-request-log to use this assertion")))
--- test/parrot/core_test.clj
+++ test/parrot/core_test.clj
@@ -34,3 +34,70 @@
             :headers {"Authorization" "Bearer ..."
                       "Content-Type" "application/json"}}))
          {:status 201})))
+
+(deftest verify-all-responses-requested--ok
+  (is (= (sut/verify-request-log
+          [{:spec [:get "https://example.com/"]}]
+          [[:get "https://example.com/"]
+           {:status 200
+            :body {:ok? true}}])
+         {:type :pass
+          :message "All response stubs were requested"
+          :expected 'empty?
+          :actual #{}})))
+
+(deftest verify-all-responses-requested--missing-request-vector
+  (is (= (sut/verify-request-log
+          [{:spec [:get "https://example.com/"]
+            :req {:method :get
+                  :url "https://example.com/"}}]
+          [[:get "https://example.com/"]
+           {:status 200
+            :body {:ok? true}}
+
+           [:post "https://example.com/"]
+           {:status 400
+            :body {:ok? false}}])
+         {:type :fail
+          :message "1 response stub was not requested:\n  POST https://example.com/\n\nReceived requests:\n  GET https://example.com/\n"
+          :expected #{}
+          :actual #{[:post "https://example.com/"]}})))
+
+(deftest verify-all-responses-requested--missing-request-map
+  (is (= (sut/verify-request-log
+          [{:spec [:get "https://example.com/"]
+            :req {:method :get
+                  :url "https://example.com/"}}]
+          [[:get "https://example.com/"]
+           {:status 200
+            :body {:ok? true}}
+
+           {:method :post
+            :url "https://example.com/"}
+           {:status 400
+            :body {:ok? false}}])
+         {:type :fail
+          :message "1 response stub was not requested:\n  POST https://example.com/\n\nReceived requests:\n  GET https://example.com/\n"
+          :expected #{}
+          :actual #{{:method :post, :url "https://example.com/"}}})))
+
+(deftest verify-all-responses-requested--unexpected-request
+  (is (= (sut/verify-request-log
+          [{:spec [:get "https://example.com/"]
+            :req {:method :get
+                  :url "https://example.com/"}}
+           {:req {:method :post
+                  :url "https://lol.com"}}]
+          [[:get "https://example.com/"]
+           {:status 200
+            :body {:ok? true}}
+
+           {:method :post
+            :url "https://example.com/"}
+           {:status 400
+            :body {:ok? false}}])
+         {:type :fail
+          :message "1 response stub was not requested:\n  POST https://example.com/\n\nReceived requests:\n  GET https://example.com/\n  \n  [UNEXPECTED] POST https://lol.com\n"
+          :expected #{}
+          :actual #{{:method :post
+                     :url "https://example.com/"}}})))
